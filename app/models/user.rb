89c6751d39d1c1b0de55b0e80775c443b6eb173e
TM0710 <t_manato@icloud.com>
2023-09-04 19:00:02 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  with_options presence: true do
+    validates :username
+    validates :password, format: {with: PASSWORD_REGEX, message: 'が無効です。文字と数字の両方を含めてください'}
+  end
 end
