4f58794e320dae8b76c742cb6ac74547b28060ca
TM0710 <t_manato@icloud.com>
2023-09-21 17:02:48 +0900

    ビュー修正

--- app/javascript/side_category.js
+++ app/javascript/side_category.js
@@ -18,4 +18,12 @@ window.addEventListener('turbo:load', function(){
       this.removeAttribute("style", "background-color" )
     });
   });
+
+  const favorite = document.querySelector(".favorite-link");
+  favorite.addEventListener('mouseover', function() {
+    this.setAttribute("style", "background-color: #EEEEEE;");
+  });
+  favorite.addEventListener('mouseout', function() {
+    this.removeAttribute("style", "background-color" )
+  });
 });
